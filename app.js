7b0a25157febf6e5609aa0a5559c74fe479450c5
shahzaibzaman567 <shahzaibzaman567@gmail.com>
2025-08-08 09:54:10 -0700

    change image and banner of cahnnel

---
+++ app.js
@@ -0,0 +1,8 @@
+let menu=document.querySelector("#menu");
+let sidebar=document.querySelector(".side-bar");
+
+
+menu.addEventListener("click",()=>{
+
+    sidebar.classList.toggle("small-sidebar")
+})
