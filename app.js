2cf32ddd5f380eb73833794af38e997442b4964f
shahzaibzaman567 <shahzaibzaman567@gmail.com>
2025-08-08 17:59:27 -0700

    add play.html file

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 let menu=document.querySelector("#menu");
 let sidebar=document.querySelector(".side-bar");
-
+let container=document.querySelector('.container');
 
 menu.addEventListener("click",()=>{
 
     sidebar.classList.toggle("small-sidebar")
+    container.classList.toggle('large-container')
 })
